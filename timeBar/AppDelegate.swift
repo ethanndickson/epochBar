73f3e8d07459de707e37b42417ae1bf94a97dd7e
Ethan Dickson <ethanndickson@gmail.com>
2021-09-03 16:38:37 +1000

    Initial Commit

---
+++ timeBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  timeBar
+//
+//  Created by Ethan Dickson on 3/9/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ timeBar/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  timeBar
+//
+//  Created by Ethan Dickson on 3/9/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
