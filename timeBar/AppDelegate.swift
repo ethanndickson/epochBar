50c13c54d8bbf760d7c2da56b40bf09b92c13319
Ethan Dickson <ethanndickson@gmail.com>
2021-09-03 19:59:54 +1000

    First Release

--- timeBar/AppDelegate.swift
+++ timeBar/AppDelegate.swift
@@ -9,18 +9,32 @@ import Cocoa
 
 @main
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
     
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        statusItem.button?.title = "epch" // Set the menu bar title
+        statusItem.button?.target = self // Make sure the button press is sent to us
+        statusItem.button?.action = #selector(showEpochBar) // on press, run a  function that creates a popover
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
 
+    @objc func showEpochBar() {
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateController(withIdentifier: "ViewController") // set the vc to the storyboard vc
+            as? ViewController else {
+                fatalError("This should never happen")
+            }
+        
+            let popoverView = NSPopover()
+            popoverView.contentViewController = vc
+            popoverView.behavior = .transient
+        popoverView.show(relativeTo: statusItem.button!.bounds, of: statusItem.button!, preferredEdge: .maxY) // display the popover
+    }
 
 }
 
--- timeBar/ViewController.swift
+++ timeBar/ViewController.swift
@@ -1,18 +1,20 @@
 //
 //  ViewController.swift
-//  timeBar
+//  epochBar
 //
 //  Created by Ethan Dickson on 3/9/21.
 //
 
 import Cocoa
 
-class ViewController: NSViewController {
+class ViewController: NSViewController{
 
+    @IBOutlet var humanTime: NSDatePicker!
+    @IBOutlet var unixTime: NSTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        humanTime.dateValue = Date() // just in case Swift didn't update the datepicker for us..
+        convertTimestamp(humanTime.dateValue)
     }
 
     override var representedObject: Any? {
@@ -21,6 +23,35 @@ class ViewController: NSViewController {
         }
     }
 
-
+    @IBAction func copyToClipboard(_ sender: Any) {
+        let currentDate = Date()
+        humanTime.dateValue = currentDate // we do this so the datepicker updates visually
+        convertTimestamp(humanTime.dateValue)
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(unixTime.stringValue, forType: .string)
+    }
+    
+    @IBAction func copyAnyToClipboard(_ sender: Any) {
+        NSPasteboard.general.clearContents()
+        NSPasteboard.general.setString(unixTime.stringValue, forType: .string)
+    }
+    @IBAction func onDatePickerChange(_ sender: NSDatePicker) {
+        convertTimestamp(humanTime.dateValue)
+    }
+    
+    @IBAction func onUnixTimeChange(_ sender: Any) {
+        convertUnixTime(unixTime.stringValue)
+    }
+    func convertTimestamp(_ input: Date) {
+        unixTime.stringValue = String(format: "%.0f", (input.timeIntervalSince1970))
+    }
+    
+    func convertUnixTime(_ input: String) {
+        guard TimeInterval(input) != nil else { return } // dont do anything if invalid int
+        let tempUnix = TimeInterval(input)
+        let tempDate = NSDate(timeIntervalSince1970: tempUnix!) // we can get rid of the optional since we have a guard(?)
+        guard let actualDate = tempDate as Date? else { return } // check that we can convert NSDate -> Date, else dont do anything
+        humanTime.dateValue = actualDate // update visually
+    }
 }
 
